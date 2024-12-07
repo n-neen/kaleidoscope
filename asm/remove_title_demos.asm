03248a6d1a12442eb503955cc6e5b8a8e2ab1f14
n-neen <rathergoodyogurt@gmail.com>
2024-12-06 21:05:57 -0500

    rooms
    hex map replacement, tourian, room 2 retile

--- asm/remove_title_demos.asm
+++ asm/remove_title_demos.asm
@@ -5,5 +5,11 @@ org $8B9F29         ;remove title demos (attract mode)
 LDA $1F53
 
 org $8289ea         ;skip hex map
-    inc $0998
+    ;inc $0998
+    ;rts
+    
+    jsl $819e3e    ;vanilla code in this spot
     rts
+    
+org $81a98a         ;hex map 'PLANET ZEBES' sprite removal
+    nop #4
